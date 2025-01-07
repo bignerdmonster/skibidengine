d0ba4e671e2adf7794651dba25519ee07b7078fd
⌬ Felix ⌬ <s216-felix.bryan@k12.hi.us>
2025-01-07 09:19:16 -1000

    inital commit ish

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://40317178ab965892cd49d8950dbaa9f8@o4508450854141952.ingest.us.sentry.io/4508450875310080",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import "~/styles/globals.css";
+
+import { GeistSans } from "geist/font/sans";
+import { type Metadata } from "next";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs';
+
+export const metadata: Metadata = {
+  title: "skibidengine | a fourm engine",
+  description: "Fent Reactor; a reactor to power the world",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <ClerkProvider>
+      <html lang="en" className={`${GeistSans.variable}`}>
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
